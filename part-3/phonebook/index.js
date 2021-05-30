9c6f1cae1d0e78ff55b7431f7c48279fe702be92
Siddarth Patel <siddarthpatel22@gmail.com>
2021-05-30 19:22:14 +0530

    enable cors to communicate with react ui

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token('body', (request) => JSON.stringify(request.body));
 app.use(morgan(function (tokens, request, response) {
